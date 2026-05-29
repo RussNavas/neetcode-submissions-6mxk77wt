59fd3f759a2ca6b9ba71aaf4038b937234214392
RussNavas <navasr@oregonstate.edu>
2026-05-29 12:33:50 -0700

    Add: binary-search - submission-10

---
+++ Data Structures & Algorithms/binary-search/submission-10.go
@@ -0,0 +1,18 @@
+func search(nums []int, target int) int {
+
+    l := 0
+    r := len(nums)-1
+    for l <= r{
+        m := (r + l) / 2
+        if nums[m] == target{
+            return m
+        }
+        if nums[m] < target{
+            l = m + 1
+        } else{
+            r = m -1
+        }
+    }
+    return -1
+
+}
