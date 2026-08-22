e8723b8981ef2c931446dd6408f8104cf9735f4e
Russell Navas <navasr@oregonstate.edu>
2026-08-21 20:46:42 -0700

    Add: clone-graph - submission-13

---
+++ Data Structures & Algorithms/clone-graph/submission-13.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        def dfs(node, og_to_clone):
+            if node in og_to_clone:
+                return og_to_clone[node]
+            if node not in og_to_clone:
+                clone = Node(node.val)
+                og_to_clone[node] = clone
+            for n in node.neighbors:
+                og_to_clone[node].neighbors.append(dfs(n, og_to_clone))
+            return og_to_clone[node]
+        og_to_clone = {}
+        dfs(node, og_to_clone)
+        return og_to_clone[node]
