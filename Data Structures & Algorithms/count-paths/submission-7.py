bbb22bcab6fe11b51c2b23f2b8aa4687ae2897e0
Russell Navas <navasr@oregonstate.edu>
2026-08-22 20:53:30 -0700

    Add: count-paths - submission-7

---
+++ Data Structures & Algorithms/count-paths/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = [[0]*n for r in range(m)]
+
+        def bfs(r, c, cache):
+            if r == m - 1 and c == n - 1:
+                return 1
+            if r == m or c == n:
+                return 0
+            
+            if cache[r][c]:
+                return cache[r][c]
+
+            cache[r][c] = (bfs(r + 1, c, cache) + bfs(r, c+1, cache))
+            return cache[r][c]
+        
+        return bfs(0, 0, cache)
