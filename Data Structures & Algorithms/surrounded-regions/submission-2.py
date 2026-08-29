099f1915f80856215545a0a4c316de19a6f0f9ed
Russell Navas <navasr@oregonstate.edu>
2026-08-28 21:13:22 -0700

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        DIRS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        q = deque()
+        for r in range(ROWS):
+            if board[r][0] == "O":
+                q.append((r, 0))
+            if board[r][COLS-1] == "O":
+                q.append((r, COLS-1))
+        
+        for c in range(COLS):
+            if board[0][c] == "O":
+                q.append((0, c))
+            if board[ROWS - 1][c] == "O":
+                q.append((ROWS - 1, c))
+        
+        while q:
+            # get border node
+            r, c = q.popleft()
+            board[r][c] = "T"
+            #explore neighbors
+            for dr, dc in DIRS:
+                nr, nc = r + dr, c + dc
+                if (min(nr, nc) < 0 or nr == ROWS or nc == COLS or
+                    board[nr][nc] != "O"):
+                    continue
+                board[nr][nc] = "T"
+                q.append((nr, nc))
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
