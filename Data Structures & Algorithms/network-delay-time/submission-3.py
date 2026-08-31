2e7a8d351467830edce7458d71b69fbe03c58eb8
Russell Navas <navasr@oregonstate.edu>
2026-08-31 13:39:16 -0700

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = {i: [] for i in range(1, n+1)}
+        for s, d, t in times:
+            adj[s].append([t, d])
+        
+        shortest = {}
+        minHeap = [[0, k]]
+        t= 0
+        while minHeap:
+            t1, n1 = heapq.heappop(minHeap)
+            if n1 in shortest:
+                continue
+            shortest[n1] = t1
+            t = t1
+            for t2, n2 in adj[n1]:
+                if n2 not in shortest:
+                    heapq.heappush(minHeap, [t1 + t2, n2])
+        
+        return t if len(shortest) == n else -1
