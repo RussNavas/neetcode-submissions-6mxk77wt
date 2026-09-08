1bb654b53b270902d014960ab1249e3716bd96b2
Russell Navas <navasr@oregonstate.edu>
2026-09-08 16:39:22 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,23 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([ i.start for i in intervals ])
+        end = sorted([ i.end for i in intervals ])
+        i, j = 0, 0
+        res, count = 0, 0
+        while i < len(start):
+            if start[i] < end[j]:
+                count += 1
+                i += 1
+            else:
+                j += 1
+                count -= 1
+            res = max(count, res)
+        return res
